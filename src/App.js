42a8f79efb5c391fcb57565a7304ab45e8d69fd0
David Ryan <djrdev@icloud.com>
2018-03-22 10:12:09 -0600

    header & navbar

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            <span>D</span>avid <span>J</span> <span>R</span>yan
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        <nav id="site-nav">
+          <div id="main-nav">
+            <a class="button" href="/">Home</a>
+            <a class="button" href="/code">Code</a>
+            <a class="button" href="/audio">Audio</a>
+          </div>
+          <div id="sub-nav">
+            <a class="button">Home</a>
+            <a class="button">Code</a>
+            <a class="button">Audio</a>
+          </div>
+        </nav>
+        <section id="content-holder">
+          
+        </section>
+      </div>;
   }
 }
 

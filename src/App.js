d2f734418270f6ecfa125f83347adb612a3bd98e
David Ryan <djrdev@icloud.com>
2018-03-24 20:07:13 -0600

    button and card html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constuctor() {
+    this.state= {
+
+    }
+  }
   render() {
     return <div className="App">
         <header className="App-header">
@@ -12,19 +16,28 @@ class App extends Component {
         </header>
         <nav id="site-nav">
           <div id="main-nav">
-            <a class="button" href="/">Home</a>
-            <a class="button" href="/code">Code</a>
-            <a class="button" href="/audio">Audio</a>
+            <a class="button home" >Home</a>
+            <a class="button code" >Code</a>
+            <a class="button sound" >Sound</a>
           </div>
           <div id="sub-nav">
             <a class="button">Home</a>
             <a class="button">Code</a>
-            <a class="button">Audio</a>
+            <a class="button">Sound</a>
           </div>
         </nav>
-        <section id="content-holder">
-          
-        </section>
+        <main id="content-holder">
+          <h2 class="content-title">Selected Works & Highlights</h2>
+          <section class="previews">
+            {/* <Route path="/" component={Home}/>
+            <Route path="/code" component={Code}/>
+            <Route path="/sound" component={Sound}/> */}
+            <article class="preview-thumb">
+              
+            </article>
+
+          </section>
+        </main>
       </div>;
   }
 }

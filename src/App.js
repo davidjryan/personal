e9bef5b157d8d8df6e0966314af236b1b120f38d
David Ryan <djrdev@icloud.com>
2018-09-26 18:19:46 -0600

    app iteration

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constuctor() {
-    this.state= {
+  constructor() {
+    super()
+    this.state = {
 
     }
   }
+
+  handleClick(id) {
+
+  }
+
   render() {
     return <div className="App">
         <main>
@@ -17,38 +23,35 @@ class App extends Component {
           </header>
           <nav id="site-nav">
             <div id="main-nav">
-              <a class="button home">Home</a>
-              <a class="button code">Code</a>
-              <a class="button sound">Sound</a>
+              <a className="button home" onClick={this.handleClick}>Home</a>
+              <a className="button code" onClick={this.handleClick}>Code</a>
+              <a className="button sound" onClick={this.handleClick}>Sound</a>
             </div>
             <div id="sub-nav">
-              <a class="button">Home</a>
-              <a class="button">Code</a>
-              <a class="button">Sound</a>
+              <a className="button">Home</a>
+              <a className="button">Code</a>
+              <a className="button">Sound</a>
             </div>
           </nav>
           <section id="content-holder">
-            <h2 class="content-title">Selected Works & Highlights</h2>
-            <section class="preview-container">
+            <h2 className="content-title">Selected Works & Highlights</h2>
+            <section className="preview-container">
               {/* <Route path="/" component={Home}/>
             <Route path="/code" component={Code}/>
             <Route path="/sound" component={Sound}/> */}
-              <article class="preview">
+              <article className="preview-thumb">
                 <a href="">
-                  <div class="preview-thumb" />
-                  <div class="preview-title" />
+                  <div className="preview-title" />
                 </a>
               </article>
-              <article class="preview">
+              <article className="preview-thumb">
                 <a href="">
-                  <div class="preview-thumb" />
-                  <div class="preview-title" />
+                  <div className="preview-title" />
                 </a>
               </article>
-              <article class="preview">
+              <article className="preview-thumb">
                 <a href="">
-                  <div class="preview-thumb" />
-                  <div class="preview-title" />
+                  <div className="preview-title" />
                 </a>
               </article>
             </section>
